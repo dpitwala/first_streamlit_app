7fec4bd7253554c0f5c849ce280828ef1b7e6e1e
dpitwala <129676531+dpitwala@users.noreply.github.com>
2023-04-02 19:51:42 +0200

    Update streamlit_app.py
    list from CSV

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Parents New Healty Diner')
 
@@ -7,3 +10,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.dataframe(my_fruit_list)
